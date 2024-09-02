b1802b284775de3eca449681df6edf87b5760a3a
Abdurahman <abdurahman@Noutbuk-Abdurahman.local>
2024-09-02 17:51:00 +0600

    feat:subjects

--- src/features/TestConstructor/ui/index.tsx
+++ src/features/TestConstructor/ui/index.tsx
@@ -1,14 +1,24 @@
+import { useEffect } from "react";
 import { TitleHead, ItemsList } from "../style/style";
 import Item from "./items/Item";
+import { useItemStore } from "./model/UseItemStore";
 
 const ItemList = () => {
+  const items = useItemStore((state) => state.items); // Получаем предметы из zustand
+  const fetchItems = useItemStore((state) => state.fetchItems); // Получаем функцию для загрузки данных
+
+  // Загружаем предметы при монтировании компонента
+  useEffect(() => {
+    fetchItems();
+  }, [fetchItems]);
+
   return (
     <div>
       <TitleHead>
         <h1>Предметы</h1>
       </TitleHead>
       <ItemsList>
-        {itemsArray.map((title, i) => (
+        {items.map((title, i) => (
           <Item name={title} key={i} />
         ))}
       </ItemsList>
@@ -17,15 +27,3 @@ const ItemList = () => {
 };
 
 export default ItemList;
-
-const itemsArray = [
-  "Кыргызский язык",
-  "Русский язык",
-  "Кыргызская литература",
-  "Русская литература",
-  "Английский язык",
-  "Математика",
-  "История",
-  "Биология",
-  "Физика",
-];
--- src/features/TestConstructor/ui/model/store.ts
+++ src/features/TestConstructor/ui/model/store.ts
@@ -1,10 +1,13 @@
 import { create } from "zustand";
-import { TestState } from "../../type";
+import { api } from "../../api";
+import { SubjectReq, TestState } from "../../type";
 
 interface StoreState {
   testArray: TestState[];
+  subjects: string[];
   setTestArray: (question: TestState) => void;
   updateQuestion: (question: TestState) => void;
+  fetchSubjects: () => void;
 }
 
 export const defaultQuestion: TestState = {
@@ -20,8 +23,19 @@ export const defaultQuestion: TestState = {
 
 export const useStore = create<StoreState>((set) => ({
   testArray: [defaultQuestion],
+  subjects: [],
   setTestArray: (question: TestState) =>
     set((state) => ({ testArray: [...state.testArray, { ...question, id: state.testArray.length + 1 }] })),
   updateQuestion: (question: TestState) =>
     set((state) => ({ testArray: state.testArray.map((x) => (x.id === question.id ? question : x)) })),
+
+  fetchSubjects: async () => {
+    const response = await api.getSubjects();
+    if (Array.isArray(response.data)) {
+      const subjects = response.data.map((subject: SubjectReq) => subject.subjectName);
+      set({ subjects }); 
+    } else {
+      console.error("Ошибка: полученные данные не являются массивом.");
+    }
+  },
 }));
